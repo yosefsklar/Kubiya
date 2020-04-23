eaaa1bd35b0a05559af5be85c6f6711eac0ddc33
yosefsklar <yosef.sklar@gmail.com>
2020-04-23 12:25:20 -0400

    Updates

---
+++ precache-manifest.f8151c551f8ac1ab24763544236c88dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe5c88f0d128ed9002d47b261e7193c",
+    "url": "/Kubya/index.html"
+  },
+  {
+    "revision": "c0a4a50f4189d69f1089",
+    "url": "/Kubya/static/css/main.4460f94a.chunk.css"
+  },
+  {
+    "revision": "4cd343e03b225211aee2",
+    "url": "/Kubya/static/js/2.6831cd9c.chunk.js"
+  },
+  {
+    "revision": "31a4b0de95e923ff3b98b41dce278106",
+    "url": "/Kubya/static/js/2.6831cd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a4a50f4189d69f1089",
+    "url": "/Kubya/static/js/main.37201c72.chunk.js"
+  },
+  {
+    "revision": "91406fea4d84d38f9dfe",
+    "url": "/Kubya/static/js/runtime-main.2228c130.js"
+  }
+]);
